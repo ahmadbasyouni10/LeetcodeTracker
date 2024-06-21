14a0903550089863dde7de216ffa6c056635ba44
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-06-21 16:39:03 -0400

    Fixed styling errors

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ response = requests.get(url)
 if response.status_code == 200:
     matches = response.json()
     matches_df = pd.DataFrame(matches['data'], columns=['id', 'name',
-                                                        'starting_at', 'result_info',
-                                                        'leg', 'length', 'has_odds'])
+                                                        'starting_at', 
+                                                        'result_info',
+                                                        'leg', 
+                                                        'length', 
+                                                        'has_odds'])
     engine = db.create_engine('sqlite:///soccergames.db')
     matches_df.to_sql('matches', con=engine, if_exists='replace', index=False)
 
