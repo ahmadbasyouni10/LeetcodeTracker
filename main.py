9811514fb765739513b11b70d05ad7baf5a40330
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-06-21 16:34:38 -0400

    fixed pycodestyle errors

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ response = requests.get(url)
 
 if response.status_code == 200:
     matches = response.json()
-    matches_df = pd.DataFrame(matches['data'], columns=['id', 'name', 
-    'starting_at', 'result_info', 'leg', 'length', 'has_odds'])
+    matches_df = pd.DataFrame(matches['data'], columns=['id', 'name',
+                                                        'starting_at', 'result_info', 'leg', 'length', 'has_odds'])
     engine = db.create_engine('sqlite:///soccergames.db')
     matches_df.to_sql('matches', con=engine, if_exists='replace', index=False)
 
     with engine.connect() as connection:
-        query_result = connection.execute(db.text('SELECT * FROM matches;')).fetchall()
+        query_result = connection.execute(
+            db.text('SELECT * FROM matches;')
+            ).fetchall()
         print(pd.DataFrame(query_result))
 else:
     print('Error:', response.status_code)
