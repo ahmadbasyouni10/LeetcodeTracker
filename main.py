123f0b742016735b53b977d99a75d737ba6f2353
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-06-21 13:30:02 -0400

    Created a sqllite database and inserted data into table

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import requests
+import pandas as pd
+import sqlalchemy as db 
+from dotenv import load_dotenv
+import os
 
-api_token = 'BoB5L7m9jHfQHCefs2H4ySjpFlr3UtHvbVSvdhMVjhi69iD1jYUvQpknbM0Q'
+load_dotenv()
+
+api_token = os.getenv('API_TOKEN')
 url = f'https://api.sportmonks.com/v3/football/fixtures?api_token={api_token}'
 
 response = requests.get(url)
 
+
 if response.status_code == 200:
-    print('GET request successful!')
-    data = response.json()
-    res = []
-    for match in data['data']:
-      res.append(match['name'])
-    print(res)
+    matches = response.json()
+    matches_df = pd.DataFrame(matches['data'], columns=['id', 'name', 'starting_at', 'result_info', 'leg', 'length', 'has_odds'])
+    engine = db.create_engine('sqlite:///soccergames.db')
+    matches_df.to_sql('matches', con=engine, if_exists='replace', index=False)
+
+    with engine.connect() as connection:
+        query_result = connection.execute(db.text('SELECT * FROM matches;')).fetchall()
+        print(pd.DataFrame(query_result))
+        
 else:
-    print('GET request failed. Status code:', response.status_code)
-    print('Response content:', response.text)
-
-
-
-
+    print('Error:', response.status_code)
     
 
