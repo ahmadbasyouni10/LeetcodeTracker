b60ae9af460b16d771ebaded8d6e247a95575763
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-07-08 12:08:34 -0400

    Implemented create and edit problem ⚡

--- frontend/src/components/CreateProblemModal.jsx
+++ frontend/src/components/CreateProblemModal.jsx
@@ -1,39 +1,111 @@
-import { Button, Modal, Radio, ModalFooter, ModalBody, Textarea, Flex, FormLabel, Input, ModalHeader, ModalCloseButton, FormControl, ModalContent, ModalOverlay, useDisclosure, RadioGroup} from "@chakra-ui/react"
+import { Button, Modal, Radio, ModalFooter, ModalBody, Textarea, Flex, FormLabel, Input, ModalHeader, ModalCloseButton, FormControl, ModalContent, ModalOverlay, useDisclosure, useToast, RadioGroup} from "@chakra-ui/react"
 import { BiAddToQueue } from "react-icons/bi"
+import { useState } from "react"
+import { BASEURL } from "../App"
 
-const CreateProblemModal = () => {
+const CreateProblemModal = ({setProblems}) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [isLoading, setIsLoading] = useState(false)
+    const [inputs, setInputs] = useState({
+        name: "",
+        difficulty: "",
+        url: "",
+        notes: "",
+        status: ""
+    })
+    const toast = useToast()
+
+    const handleChange = (e) => {   
+        const {name, value} = e.target
+        setInputs((prevInputs) => ({...prevInputs, [name]: value}))
+    }
+
+    const handleCreateProblem = async (e) => {
+        e.preventDefault()
+        setIsLoading(true)
+        try {
+            const res = await fetch(BASEURL + '/problems', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(inputs)
+
+            })
+            const data = await res.json()
+            if(!res.ok) {
+                throw new Error(data.error)
+            }
+            toast({
+                title: "W in the chat 🎉",
+                description: "Your problem has been added successfully",
+                status: "success",
+                duration: 2000,
+                position: "top-center"
+            })
+            onClose();
+            setProblems((prevProblems) => [...prevProblems, data])
+            setInputs({
+                name: "",
+                difficulty: "",
+                url: "",
+                notes: "",
+                status: ""
+            })
+        }
+        catch (error) {
+            toast({
+                title: "Error",
+                description: error.message,
+                status: "error",
+                duration: 2000,
+                position: "top-center"
+            })
+        }
+        finally {
+            setIsLoading(false)
+
+    }
+}
+
     return (
     <>
         <Button onClick={onOpen}>
             <BiAddToQueue size={20} />
         </Button>
 
-        <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        >
-            <ModalOverlay />
-            <ModalContent>
+        <Modal isOpen={isOpen} onClose={onClose}>
+ 				<ModalOverlay />
+                <form onSubmit={handleCreateProblem}>
+ 				<ModalContent>
                 <ModalHeader>My new LC problem 😍</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody pb={6}>
                     <Flex alignItems={"center"} gap={4}>
                         <FormControl>
                             <FormLabel>Problem Name</FormLabel>
-                            <Input placeholder="Two Sum" />
+                            <Input placeholder="Two Sum" 
+                            name="name"
+                            onChange={handleChange}
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>Difficulty</FormLabel>
-                            <Input placeholder="Easy" />
+                            <Input placeholder="Easy"
+                            name="difficulty"
+                            onChange={handleChange}
+                             />
                         </FormControl>
                     </Flex>
                     
                     <Flex alignItems={"center"} mt={4} gap={4}>
                         <FormControl>
                             <FormLabel>Problem URL</FormLabel>
-                            <Input placeholder="Easy" />
+                            <Input placeholder="https://leetcode.com/problems/two-sum/" 
+                            name="url"
+                            onChange={handleChange}
+                            />
                         </FormControl>
                     </Flex>
 
@@ -42,26 +114,38 @@ const CreateProblemModal = () => {
                             <Textarea 
                             resize={"none"}
                             overflowY={"hidden"}
-                            placeholder="Use a hashmap" />
+                            placeholder="Use a hashmap"
+                            name="notes" 
+                            onChange={handleChange}
+                            />
                         </FormControl>
 
                         <RadioGroup mt={4}>
                             <Flex gap={4}>
-                                <Radio value="Yes">Solved ✔️</Radio>
-                                <Radio value="No">Unsolved ❌</Radio>
+                                <Radio value="Solved"
+                                onChange={(e) => setInputs({...inputs, status: e.target.value})}
+                                >Solved ✔️</Radio>
+                                <Radio value="In Progress"
+                                onChange={(e) => setInputs({...inputs, status: e.target.value})}
+                                >In Progress 🔨</Radio>
+                                <Radio value="Unsolved"
+                                onChange={(e) => setInputs({...inputs, status: e.target.value})}
+                                >Unsolved ❌</Radio>
                             </Flex>
                         </RadioGroup>
                 </ModalBody>
 
                 <ModalFooter>
-                    <Button colorScheme="blue" mr={3}>
+                    <Button colorScheme="blue" mr={3} type="submit"
+                        isLoading={isLoading}
+                    >
                         Add
                     </Button>
                     <Button onClick={onClose}>Cancel</Button>
                 </ModalFooter>
             </ModalContent>
-
-        </Modal>
+            </form>
+ 			</Modal>
     </>
     )
 }
--- frontend/src/components/EditModal.jsx
+++ frontend/src/components/EditModal.jsx
@@ -12,16 +12,74 @@ import {
 	ModalFooter,
 	ModalHeader,
 	ModalOverlay,
+    useToast,
     Radio,
     RadioGroup,
 	Textarea,
 	useDisclosure,
 } from "@chakra-ui/react";
+import e from "cors";
 import { BiEditAlt } from "react-icons/bi";
+import { useState } from "react";
 
 
- function EditModal({ problem }) {
+ function EditModal({ problem, setProblems}) {
  	const { isOpen, onOpen, onClose } = useDisclosure();
+    const [isLoading, setIsLoading] = useState(false)
+    const toast = useToast()
+    const [inputs, setInputs] = useState({
+        name: problem.name,
+        difficulty: problem.difficulty,
+        url: problem.url,
+        notes: problem.notes,
+        status: problem.status
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setInputs((prevInputs) => ({...prevInputs, [name]: value}))
+    }
+
+    const handleEditProblem = async (e) => {
+        e.preventDefault()
+        setIsLoading(true)
+        try {
+            const res = await fetch('http://localhost:5000/api/problems/' + problem.id, {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(inputs)
+            })
+            const data = await res.json()
+            if(!res.ok) {
+                throw new Error(data.error)
+            }
+            setProblems((prevProblems) => prevProblems.map((p) => p.id === problem.id ? data : p))
+            toast({ 
+                title: "Problem updated",
+                description: "Your problem has been updated successfully",
+                status: "success",
+                duration: 2000,
+                position: "top-center",
+                isClosable: true
+        })
+            onClose()
+        }
+        catch (error) {
+            toast({
+                title: "Error",
+                description: error.message,
+                status: "error",
+                duration: 2000,
+                position: "top-center",
+                isClosable: true
+            })
+        }
+        finally {
+            setIsLoading(false)
+        }
+    }
 
  	return (
  		<>
@@ -34,8 +92,9 @@ import { BiEditAlt } from "react-icons/bi";
  				icon={<BiEditAlt size={20} />}
  			/>
 
- 			<Modal isOpen={isOpen} onClose={onClose}>
+<Modal isOpen={isOpen} onClose={onClose}>
  				<ModalOverlay />
+                <form onSubmit={handleEditProblem}>
  				<ModalContent>
                 <ModalHeader>My new LC problem 😍</ModalHeader>
                 <ModalCloseButton />
@@ -43,19 +102,31 @@ import { BiEditAlt } from "react-icons/bi";
                     <Flex alignItems={"center"} gap={4}>
                         <FormControl>
                             <FormLabel>Problem Name</FormLabel>
-                            <Input placeholder="Two Sum" />
+                            <Input placeholder="Two Sum" 
+                            name="name"
+                            value = {inputs.name}
+                            onChange={handleChange}
+                            />
                         </FormControl>
 
                         <FormControl>
                             <FormLabel>Difficulty</FormLabel>
-                            <Input placeholder="Easy" />
+                            <Input placeholder="Easy"
+                            name="difficulty"
+                            value = {inputs.difficulty}
+                            onChange={handleChange}
+                             />
                         </FormControl>
                     </Flex>
                     
                     <Flex alignItems={"center"} mt={4} gap={4}>
                         <FormControl>
                             <FormLabel>Problem URL</FormLabel>
-                            <Input placeholder="Easy" />
+                            <Input placeholder="https://leetcode.com/problems/two-sum/" 
+                            name="url"
+                            value = {inputs.url}
+                            onChange={handleChange}
+                            />
                         </FormControl>
                     </Flex>
 
@@ -64,24 +135,38 @@ import { BiEditAlt } from "react-icons/bi";
                             <Textarea 
                             resize={"none"}
                             overflowY={"hidden"}
-                            placeholder="Use a hashmap" />
+                            placeholder="Use a hashmap"
+                            name="notes" 
+                            value = {inputs.notes}
+                            onChange={handleChange}
+                            />
                         </FormControl>
 
-                        <RadioGroup mt={4}>
+                        <RadioGroup mt={4} value={inputs.status}>
                             <Flex gap={4}>
-                                <Radio value="Yes">Solved ✔️</Radio>
-                                <Radio value="No">Unsolved ❌</Radio>
+                                <Radio value="Solved"
+                                onChange={(e) => setInputs({...inputs, status: e.target.value})}
+                                >Solved ✔️</Radio>
+                                <Radio value="In Progress"
+                                onChange={(e) => setInputs({...inputs, status: e.target.value})}
+                                >In Progress 🔨</Radio>
+                                <Radio value="Unsolved"
+                                onChange={(e) => setInputs({...inputs, status: e.target.value})}
+                                >Unsolved ❌</Radio>
                             </Flex>
                         </RadioGroup>
                 </ModalBody>
 
                 <ModalFooter>
-                    <Button colorScheme="blue" mr={3}>
+                    <Button colorScheme="blue" mr={3} type="submit"
+                        isLoading={isLoading}
+                    >
                         Update
                     </Button>
                     <Button onClick={onClose}>Cancel</Button>
                 </ModalFooter>
             </ModalContent>
+            </form>
  			</Modal>
  		</>
  	);
